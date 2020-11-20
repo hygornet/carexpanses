5ef3ef1792479745cc473b66dc13bfd1a566635f
Hygor <santos.hygor@outlook.com.br>
2020-11-19 21:32:41 -0300

    ajustes tela alcool_gasolina

--- lib/views/alcool_gasolina.dart
+++ lib/views/alcool_gasolina.dart
@@ -8,15 +8,26 @@ class AlcoolGasolina extends StatefulWidget {
 }
 
 String resultado;
+var img = '';
 bool isValid = false;
+var _formKey = GlobalKey<FormState>();
+
+var alcoolController = MoneyMaskedTextController(
+  decimalSeparator: '.',
+);
+var gasolinaController = MoneyMaskedTextController(
+  decimalSeparator: '.',
+);
+String informacaoMelhorAbastecerCom =
+    "O cálculo básico para se descobrir se o álcool é vantajoso ou não em relação à gasolina é simples. Basta dividir o preço do litro do etanol pelo da gasolina. Se o resultado for inferior a 0,7, o álcool é o melhor para abastecer. Se for maior que 0,7, então a gasolina é melhor.";
 
 String melhorAbastecerCom(double valorAlcool, double valorGasolina) {
   double resultGasolina = valorGasolina * 0.7;
   var r = double.parse(resultGasolina.toStringAsFixed(2));
   if (r < valorAlcool) {
-    resultado = 'Complete com alcool';
+    resultado = 'Abasteça com álcool!';
   } else if (r > valorAlcool) {
-    resultado = 'Complete com gasolina';
+    resultado = 'Abasteça com gasolina!';
   } else if (r == valorAlcool) {
     resultado = 'Os dois tem o mesmo custo benefício.';
   }
@@ -27,13 +38,23 @@ class _AlcoolGasolinaState extends State<AlcoolGasolina> {
   void limpar() {
     setState(() {
       resultado = "";
+      img = '';
+      alcoolController.clear();
+      gasolinaController.clear();
     });
+  }
+
+  void validarFormulario() {
+    _formKey.currentState.validate();
   }
 
   @override
   Widget build(BuildContext context) {
-    var alcoolController = MoneyMaskedTextController(decimalSeparator: '.');
-    var gasolinaController = MoneyMaskedTextController(decimalSeparator: '.');
+    Future sair() async {
+      isValid = false;
+      Navigator.of(context).pop();
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -57,64 +78,83 @@ class _AlcoolGasolinaState extends State<AlcoolGasolina> {
           color: Colors.black,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Form(
-                child: Column(
-                  children: [
-                    Input(
-                      texto: 'Alcool',
-                      controller: alcoolController,
-                      func: limpar,
-                    ),
-                    Input(
-                      texto: 'Gasolina',
-                      controller: gasolinaController,
-                    ),
-                    Container(
-                      height: 40,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(10),
+      body: WillPopScope(
+        onWillPop: () async {
+          isValid = false;
+          Navigator.of(context).pop();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Text(informacaoMelhorAbastecerCom),
+                      SizedBox(height: 20),
+                      Input(
+                        texto: 'Alcool',
+                        controller: alcoolController,
+                        func: limpar,
                       ),
-                      child: FlatButton(
-                        child: Text('Calcular'),
-                        onPressed: () {
-                          setState(() {
-                            resultado = melhorAbastecerCom(
-                                double.parse(alcoolController.text),
-                                double.parse(gasolinaController.text));
-                            isValid = true;
-                          });
-                        },
+                      Input(
+                        texto: 'Gasolina',
+                        controller: gasolinaController,
                       ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    isValid
-                        ? Container(
-                            width: double.infinity,
-                            child: Column(
-                              children: [
-                                Image.asset(
-                                  'lib/assets/check-clipart-gif-animation-18-unscreen.gif',
-                                  height: 100,
-                                ),
-                                Text(resultado.toString()),
-                              ],
-                            ),
-                          )
-                        : Container(),
-                  ],
+                      SizedBox(height: 20),
+                      Container(
+                        height: 40,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: FlatButton(
+                          child: Text('Calcular'),
+                          onPressed: () {
+                            setState(() {
+                              resultado = melhorAbastecerCom(
+                                  double.parse(alcoolController.text),
+                                  double.parse(gasolinaController.text));
+                              isValid = true;
+                              img =
+                                  'lib/assets/check-clipart-gif-animation-18-unscreen.gif';
+                              validarFormulario();
+                            });
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      isValid
+                          ? Container(
+                              width: double.infinity,
+                              child: Column(
+                                children: [
+                                  Image.asset(
+                                    img,
+                                    height: 100,
+                                  ),
+                                  Text(
+                                    resultado.toString(),
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          : Container(),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
